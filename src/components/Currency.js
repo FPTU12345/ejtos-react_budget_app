f00ec8e6ba510fb7fdda330f816a0eded417562a
FPTU12345 <114289940+FPTU12345@users.noreply.github.com>
2023-08-20 11:54:48 +0700

    Update Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -2,35 +2,41 @@ import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Currency = () => {
-  const { dispatch } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
 
-  const changeLocation = (val) => {
-    dispatch({
-      type: "CHG_CURRENCY",
-      payload: val,
-    });
-  };
+    const handleCurrency = (currency) => {
+        dispatch({
+            type: 'CHG_CURRENCY', 
+            payload: currency
+        })
+    }
 
-  return (
-    <div className="alert alert-secondary">
-      {" "}
-      Currency{" "}
-      {
-        <select
-          name="Currency"
-          id="Currency"
-          onChange={(event) => changeLocation(event.target.value)}
-        >
-          <option defaultValue>Choose...</option>
-
-          <option value="$">$ Dollar</option>
-          <option value="£">£ Pound</option>
-          <option value="€">€ Euro</option>
-          <option value="₹">₹ Ruppee</option>
-        </select>
-      }
-    </div>
-  );
+    return (
+        <div style={{
+            backgroundColor: 'lightgreen', 
+            paddingLeft: '3px', 
+            height: '3rem', 
+            textAlign: 'center',
+            paddingTop: '0.65rem',
+            }}
+            className="text-white">
+            <label htmlFor="CurrencySelect">Currency: </label>
+            <select style={{
+                        width: '100px', 
+                        backgroundColor: 'lightgreen',
+                        paddingLeft: '7px', 
+                        marginLeft: '5px'                        
+                    }}
+                    className="text-white"
+                    id="CurrencySelect" 
+                    onChange={(e) => handleCurrency(e.target.value)}>
+                <option className="text-black" value='$' name="dolar">$ Dollar</option>
+                <option className="text-black" value='£' selected name="pound">£ Pound</option>
+                <option className="text-black" value='€' name="euro">€ Euro</option>
+                <option className="text-black" value='₹' name="ruppee">₹ Ruppee</option>
+            </select>
+        </div>
+    )
 };
 
 export default Currency;

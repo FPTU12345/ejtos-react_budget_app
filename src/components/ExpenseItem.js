6f1778fbf2a83a77496c15bd9ddf199975501cd4
FPTU12345 <114289940+FPTU12345@users.noreply.github.com>
2023-08-20 11:55:10 +0700

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,69 +1,51 @@
 import React, { useContext } from "react";
-
-import { TiDelete } from "react-icons/ti";
-import { FaPlusCircle } from "react-icons/fa";
-import { FaMinusCircle } from "react-icons/fa";
 import { AppContext } from "../context/AppContext";
+import { TiDelete } from "react-icons/ti";
+import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
 
 const ExpenseItem = (props) => {
-  const { dispatch, Currency } = useContext(AppContext);
+    const { currency, dispatch } = useContext(AppContext);
 
-  const handleDeleteExpense = () => {
-    dispatch({
-      type: "DELETE_EXPENSE",
-      payload: props.id,
-    });
-  };
-
-  const increaseAllocation = (name) => {
-    const expense = {
-      name: name,
-      cost: 10,
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id
+        });
     };
 
-    dispatch({
-      type: "ADD_EXPENSE",
-      payload: expense,
-    });
-  };
+    const increaseAllocation = (name) => {
+        const expense = {
+            name: name, 
+            cost: 10
+        }
 
-  const decreaseAllocation = (name) => {
-    const expense = {
-      name: name,
-      cost: -10,
-    };
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        })
+    }
 
-    dispatch({
-      type: "ADD_EXPENSE",
-      payload: expense,
-    });
-  };
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name, 
+            cost: 10
+        }
 
-  return (
-    <tr>
-      <td>{props.name}</td>
-      <td>
-        {Currency} {props.cost}
-      </td>
-      <td>
-        <FaPlusCircle
-          size="1.5em"
-          color="green"
-          onClick={(event) => increaseAllocation(props.name)}
-        ></FaPlusCircle>
-      </td>
-      <td>
-        <FaMinusCircle
-          size="1.5em"
-          color="red"
-          onClick={(event) => decreaseAllocation(props.name)}
-        ></FaMinusCircle>
-      </td>
-      <td>
-        <TiDelete size="2.2em" onClick={handleDeleteExpense}></TiDelete>
-      </td>
-    </tr>
-  );
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        })
+    }
+
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{currency}{props.cost}</td>
+            <td><AiFillPlusCircle color="green" size='2em' onClick={e => increaseAllocation(props.name)}>+</AiFillPlusCircle></td>
+            <td><AiFillMinusCircle color="red" size='2em' onClick={e => decreaseAllocation(props.name)}>-</AiFillMinusCircle></td>
+            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        </tr>
+    )
 };
 
 export default ExpenseItem;
